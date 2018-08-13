ee3bb4d1fd19055d16263167a0534790a65e2442
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-08-13 17:11:21 +0100

    add #touch_in + gTest

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,12 @@
 class Oystercard
 
-attr_reader :balance, :MAXIMUM_BALANCE
+attr_reader :balance, :MAXIMUM_BALANCE, :in_journey
 
 MAXIMUM_BALANCE = 90
 
-  def initialize(balance = 0)
+  def initialize(balance = 0, in_journey = false)
     @balance = balance
+    @in_journey = in_journey
   end
 
   def top_up(amount)
@@ -14,7 +15,11 @@ MAXIMUM_BALANCE = 90
   end
 
   def deduct(fare)
-    @balance -= fare 
+    @balance -= fare
+  end
+
+  def touch_in
+    @in_journey = true
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,25 +5,25 @@ describe Oystercard do
   let(:mockBalance) { double :balance }
   let(:mockAmount) { double :amount }
   let(:mockFare) { double :fare }
- 
+
   describe '#initialize' do
     it 'defaults with a balance of £0' do
       expect(subject.balance).to eq 0
     end
   end
 
-  describe '#top_up' do 
+  describe '#top_up' do
     it { is_expected.to respond_to(:top_up) }
-    it 'enables users to add funds' do 
-      allow(subject).to receive(:top_up).and_return(mockAmount)  # number = 10 
+    it 'enables users to add funds' do
+      allow(subject).to receive(:top_up).and_return(mockAmount)  # number = 10
       expect(subject.top_up(mockAmount)).to eq(mockAmount)       # expect(subject.top_up(10)).to eq 10
     end
-    it 'raises error if trying to exceed maximum balance' do 
+    it 'raises error if trying to exceed maximum balance' do
       expect { subject.top_up(Oystercard::MAXIMUM_BALANCE) }.to raise_error "ERROR - Exceeds maximum balance of £#{Oystercard::MAXIMUM_BALANCE}"
     end
   end
 
-  describe '#deduct' do 
+  describe '#deduct' do
     it { is_expected.to respond_to(:deduct) }
     it 'deducts fare from balance' do
       # subject.top_up(25)
@@ -32,6 +32,13 @@ describe Oystercard do
       allow(subject).to receive(:top_up).and_return(mockAmount)    # subject.top_up(25)
       allow(subject).to receive(:deduct).and_return(mockBalance)   # testFare = 2
       expect(subject.deduct(mockFare)).to eq(mockBalance)          # expect(subject.deduct(testFare)).to eq 23
+    end
+  end
+
+  describe '#touch_in' do
+    it { is_expected. to respond_to(:touch_in) }
+    it 'toggles #in_journey? status to true' do
+      expect(subject.touch_in).to eq true
     end
   end
 
